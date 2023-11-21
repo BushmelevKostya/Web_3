1a064e2fb9661327b64082fa591942b76d88ca71
Kostya <kostikbushmelev@gmail.com>
2023-11-21 17:59:56 +0300

    add dotes with click

--- src/main/webapp/resources/script/touch-shedule.js
+++ src/main/webapp/resources/script/touch-shedule.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("shedule");
-canvas.addEventListener('click', function () {
+canvas.addEventListener('click', function (e) {
     console.log(1)
     let R = "R";
     const RData = document.getElementById("form:r").value;
@@ -8,7 +8,11 @@ canvas.addEventListener('click', function () {
         alert("Impossible to define coordinate! Try choosing a radius")
     } else {
         R = Number(R) / 2;
-        const x = (((event.offsetX - 240) / 80) * R).toFixed(4);
+        const x = (((event.offsetX - 240) / 80) * R).toFixed(0);
+        console.log(x)
         const y = ((-(event.offsetY - 250) / 80) * R).toFixed(4);
+        document.getElementById('form:x').value = x;
+        document.getElementById('form:y').value = y;
+        document.getElementById("form:submit-button").click();
     }
 });
