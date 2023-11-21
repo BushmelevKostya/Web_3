4fa7cd45a3171fdc453cfb018fcee8272a7ce433
Kostya <kostikbushmelev@gmail.com>
2023-11-22 02:24:11 +0300

    update blocks

--- src/main/webapp/resources/script/draw-dots.js
+++ src/main/webapp/resources/script/draw-dots.js
@@ -1,5 +1,4 @@
 function drawDots(listX, listY, listR, listResult) {
-console.log(1)
     for (let i = 0; i < listX.length; i++) {
         drawDot(listX[i], listY[i], listR[i], listResult[i])
     }
